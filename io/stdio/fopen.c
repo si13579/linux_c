bc347a5290e140303bc2878eee6aaa86222e2040
lisi <si13579@163.com>
2021-11-03 00:17:11 +0800

    20211102

--- io/stdio/fopen.c
+++ io/stdio/fopen.c
@@ -1,17 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<errno.h>
+#include<string.h>
 
 int main()
 {
 	FILE *fp;
-	fp = fopen("tmp");
+	fp = fopen("tmp","r");
 	if(fp == NULL){
-		fprintf(stderr,"fopen(): failed!errno = %d\n",errno);
+//		fprintf(stderr,"fopen(): failed!errno = %d\n",errno);
+		perror("fopen()");
+//		fprintf(stderr,"fopen():%s\n",strerror(errno));
 		exit(1);
 	}
 
 	puts("OK");
-
+	
+	fclose(fp);
 
 
 
